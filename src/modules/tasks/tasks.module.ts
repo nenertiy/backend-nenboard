31398de2f613904dec10edf8bd5911ffcf983234
nenertiy <nenertiy@gmail.com>
2025-03-24 20:30:16 +0300

    integrate activity logging

--- src/modules/tasks/tasks.module.ts
+++ src/modules/tasks/tasks.module.ts
@@ -3,8 +3,10 @@ import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { TasksRepository } from './tasks.repository';
 import { PrismaService } from '../app/prisma.service';
+import { ActivityLogModule } from '../activity-log/activity-log.module';
 
 @Module({
+  imports: [ActivityLogModule],
   controllers: [TasksController],
   providers: [TasksService, TasksRepository, PrismaService],
   exports: [TasksService],
